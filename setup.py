20a851534382fa5bd612aec3a045f67b01d2dabe
Viktor Hercinger <herczy@balabit.hu>
2014-10-12 14:08:03 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 try:
     from setuptools import setup
+
 except ImportError:
     from distutils.core import setup
 
-setup(name = "ply",
-            description="Python Lex & Yacc",
-            long_description = """
+setup(
+    name="ply-2",
+    description="Python Lex & Yacc - second version",
+    long_description = """
 PLY is yet another implementation of lex and yacc for Python. Some notable
 features include the fact that its implemented entirely in Python and it
 uses LALR(1) parsing which is efficient and well suited for larger grammars.
@@ -15,17 +17,16 @@ productions, precedence rules, error recovery, and support for ambiguous grammar
 
 PLY is extremely easy to use and provides very extensive error checking. 
 It is compatible with both Python 2 and Python 3.
+
+This is a refactored and updated version. The origical is called ply and was
+written by David Beazley <dave@dabeaz.com>.
 """,
-            license="""BSD""",
-            version = "3.4",
-            author = "David Beazley",
-            author_email = "dave@dabeaz.com",
-            maintainer = "David Beazley",
-            maintainer_email = "dave@dabeaz.com",
-            url = "http://www.dabeaz.com/ply/",
-            packages = ['ply'],
-            classifiers = [
-              'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 2',
-              ]
-            )
+    license="BSD",
+    version="0.1",
+    author="Hercinger Viktor",
+    author_email="hercinger.viktor@gmail.com",
+    maintainer="Hercinger Viktor",
+    maintainer_email="hercinger.viktor@gmail.com",
+    url="http://www.dabeaz.com/ply/",
+    packages=['ply', 'ply.lex', 'ply.yacc'],
+)
